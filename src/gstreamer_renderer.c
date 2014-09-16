911cb8c6185060330618430147dd339e34ca9b86
Pod <monsieur.pod@gmail.com>
2014-09-16 20:23:49 +1000

    Removed no-longer-functional GStreamer video overlay window assignment

--- src/gstreamer_renderer.c
+++ src/gstreamer_renderer.c
@@ -17,10 +17,7 @@
  */
 
 #include "gstreamer_renderer.h"
-#include <bcm_host.h>
-#include <EGL/egl.h>
 #include <gst/gst.h>
-#include <gst/video/video.h>
 #include <glib.h>
 #include <stdlib.h>
 #include <string.h>
@@ -116,28 +113,18 @@ static const char * oculus_rift_frag_shader =
 
 const char * shader_source_tmp_path = "/tmp/raspifpv-shader.frag";
 
-static const int LAYER_NUMBER = 0;
-
 struct _FPVGStreamerRenderer {
     GstPipeline * pipeline;
-    DISPMANX_ELEMENT_HANDLE_T dispman_element;
-    DISPMANX_DISPLAY_HANDLE_T dispman_display;
 };
 
 static const char * GST_PIPELINE_RECEIVE = "udpsrc %s port=%d caps=\"application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264\" ! rtph264depay";
 static const char * GST_PIPELINE_DECODE = "h264parse ! omxh264dec";
 static const char * GST_PIPELINE_SHADER = "glshader name=shader";
-static const char * GST_PIPELINE_SINK = "glimagesink name=sink";
+static const char * GST_PIPELINE_SINK = "glimagesink sync=false name=sink";
 
-typedef struct
-{
-  EGL_DISPMANX_WINDOW_T w;
-  DISPMANX_DISPLAY_HANDLE_T d;
-} RPIWindowData;
+#pragma mark -
+#pragma mark Forward declarations
 
-#pragma mark - Forward declarations
-
-static int fpv_gstreamer_renderer_init_window(FPVGStreamerRenderer * renderer);
 static gboolean on_message(GstBus * bus, GstMessage * message, gpointer user_data);
 
 #pragma mark -
@@ -179,8 +166,6 @@ FPVGStreamerRenderer * fpv_gstreamer_renderer_new(GMainLoop * loop, char * multi
     fclose(fd);
     g_object_set(shader, "location", shader_source_tmp_path, NULL);
     
-    fpv_gstreamer_renderer_init_window(renderer);
-
     printf("Listening for video at %s:%d\n", multicast_addr && strlen(multicast_addr) > 0 ? multicast_addr : "0.0.0.0", port);
     
     GstBus *bus = gst_pipeline_get_bus(GST_PIPELINE(renderer->pipeline));
@@ -193,10 +178,7 @@ FPVGStreamerRenderer * fpv_gstreamer_renderer_new(GMainLoop * loop, char * multi
 
 void fpv_gstreamer_renderer_dispose(FPVGStreamerRenderer * renderer) {
     gst_object_unref(renderer->pipeline);
-    DISPMANX_UPDATE_HANDLE_T dispman_update = vc_dispmanx_update_start(0);
-    vc_dispmanx_element_remove(dispman_update, renderer->dispman_element);
-    vc_dispmanx_update_submit_sync(dispman_update);
-    vc_dispmanx_display_close(renderer->dispman_display);
+    free(renderer);
 }
 
 void fpv_gstreamer_renderer_start(FPVGStreamerRenderer * renderer) {
@@ -205,53 +187,6 @@ void fpv_gstreamer_renderer_start(FPVGStreamerRenderer * renderer) {
 
 void fpv_gstreamer_renderer_stop(FPVGStreamerRenderer * renderer) {
     gst_element_set_state(GST_ELEMENT(renderer->pipeline), GST_STATE_NULL);
-}
-
-static int fpv_gstreamer_renderer_init_window(FPVGStreamerRenderer * renderer) {
-    bcm_host_init();
-    
-    uint32_t display_width;
-    uint32_t display_height;
-    int ret = graphics_get_display_size(0, &display_width, &display_height);
-    if ( ret < 0 ) {
-        fprintf(stderr, "FPVGStreamerRenderer: Can't open display\n");
-        return 0;
-    }
-
-    VC_RECT_T dst_rect = { .x = 0, .y = 0, .width = display_width, .height = display_height };
-    VC_RECT_T src_rect = { .x = 0, .y = 0, .width = display_width << 16, .height = display_height << 16 };
-
-    DISPMANX_DISPLAY_HANDLE_T display = vc_dispmanx_display_open(0);
-    if ( !display ) {
-        fprintf(stderr, "FPVGStreamerRenderer: Can't open display\n");
-        return 0;
-    }
-    DISPMANX_UPDATE_HANDLE_T update = vc_dispmanx_update_start(0);
-    DISPMANX_ELEMENT_HANDLE_T element = vc_dispmanx_element_add(update, display, LAYER_NUMBER, 
-                                            &dst_rect, 0, &src_rect, DISPMANX_PROTECTION_NONE, 
-                                            0, 0, (DISPMANX_TRANSFORM_T)0);
-    vc_dispmanx_update_submit_sync(update);
-
-    if ( !element ) {
-        fprintf(stderr, "FPVGStreamerRenderer: Can't setup dispmanx element\n");
-        vc_dispmanx_display_close(display);
-        return 0;
-    }
-    
-    GstElement *sink = gst_bin_get_by_name(GST_BIN(renderer->pipeline), "sink");
-    g_assert(sink);
-    
-    RPIWindowData * window_data = g_slice_new0(RPIWindowData);
-    window_data->d = display;
-    window_data->w.element = element;
-    window_data->w.width = display_width;
-    window_data->w.height = display_height;
-    gst_video_overlay_set_window_handle(GST_VIDEO_OVERLAY(sink), (guintptr)window_data);
-    
-    renderer->dispman_display = display;
-    renderer->dispman_element = element;
-
-    return 1;
 }
 
 static gboolean on_message(GstBus * bus, GstMessage * message, gpointer user_data) {
